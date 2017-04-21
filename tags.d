d7833803d8d1955c01e51d1a60f59cdaf319d181
Sergei Degtiarev <sdegtiarev@yahoo.com>
2017-04-21 13:03:01 -0400

    tags finished

--- tags.d
+++ tags.d
@@ -1,28 +1,23 @@
-module tagged;
 import std.string;
 import std.regex;
 import std.range;
 import std.algorithm;
 import std.exception;
+import std.datetime;
+import std.conv;
 import std.stdio;
 import loader;
+import tagged;
 
 void main(string[] arg)
 {
+	auto t0=now();
 	string page=(arg.length > 1)? load(arg[1]) : load(stdin);
-	auto top=section!"div"(page)[0];
+	auto top=section!"div"(page);
 
-	//foreach(sub; top["A"]) {
-	//	writeln(sub.name,"(",sub.opt["id"],")");
-	//	foreach(x; sub["B"])
-	//		writeln("\t",x.name,"(",x.opt["id"],")");
-	//}
-	
-	////top.print();
-	//foreach(sub; top.children.filter!(a => "class" in a.opt))
-	//	writeln(sub.opt["class"]);
 
 	auto post=top
+		["layout"]
 		["layout__row layout__row_body"]
 		["layout__cell layout__cell_body"]
 		["column-wrapper column-wrapper_lists js-sticky-wrapper"]
@@ -31,124 +26,141 @@ void main(string[] arg)
 		["posts shortcuts_items"]
 		;
 	foreach(it; post.list!"class"("post post_teaser shortcuts_item")) {
-		writeln("post ", it.opt["id"][5..$]);
-		auto hdr=it["post__header"].text;
-		auto dt=section!"span"(hdr);
-		writeln(strip(dt[0].text));
+		//writeln("post ", it.opt["id"][5..$]);
+		auto dt=section!"span"(it["post__header"].text);
+		if(strip(dt.at("post__time_published").text).empty)
+			continue;
+		//writeln(strip(dt["post__time_published"].text));
 
 		auto info=it["post__footer"]["infopanel_wrapper js-user_"];
-		writeln("\tviews ", info["views-count_post"].text);
+		//writeln("\tviews ", info["views-count_post"].text);
 
-		auto spn=info["favorite-wjt favorite"].text;
-		Section!"span" st;
-		st.children=section!"span"(spn);
-		writeln("\tstarred ", st["favorite-wjt__counter js-favs_count"].text);
+		auto st=section!"span"(info["favorite-wjt favorite"].text);
+		//writeln("\tstarred ", st["favorite-wjt__counter js-favs_count"].text);
 
-		auto cmt=info["post-comments"].text;
-		Section!"a" cm;
-		cm.children=section!"a"(cmt);
-		writeln("\tcomments ", cm.at("post-comments__link post-comments__link_all").text);
+		auto cm=section!"a"(info["post-comments"].text);
+		auto ncm=cm.at("post-comments__link post-comments__link_all").text;
+		if(ncm.empty) ncm="0";
+		//writeln("\tcomments ", cm.at("post-comments__link post-comments__link_all").text);
+
+		writefln("%-8s %s  %-12s    %-6s    %-4s %4s"
+			, it.opt["id"][5..$]
+			, at(strip(dt.at("post__time_published").text), t0)
+			//, t0
+			, (t0-at(strip(dt.at("post__time_published").text), t0)).total!"seconds"
+			, views(info["views-count_post"].text)
+			, st["favorite-wjt__counter js-favs_count"].text
+			, ncm
+		);
 
 	}
 
 }
 
 
-
-Section!TAG[] section(string TAG)(ref string page)
+int views(string s)
 {
-	Section!TAG[] lst;
-	while(1) {
-		Section!TAG s;
-		auto r=matchFirst(page, ctRegex!(Section!TAG.any, "is"));
-		if(!r) {
-			swap(s.prefix, page);
-			lst~=s;
-			break;
-		} else if(r[0] == Section!TAG.end) {
-			s.prefix=r.pre;
-			page=r.post;
-			lst~=s;
-			break;
-		} else /* if(r[0] == Section!TAG.begin) */{
-			s.prefix=r.pre;
-
-			auto op=matchFirst(r.post, ctRegex!(Section!TAG.options, "s"));
-			enforce(op, "section header not terminated");
-			s.opt=options(op[1]);
-
-			page=op.post;
-			s.children=section!TAG(page);
-			lst~=s;
-
-		}
+	if(s[$-1..$] == "k") {
+		auto n=indexOf(s, ',');
+		if(n > 0)
+			return cast(int)(1000*to!float(s[0..n]~"."~s[n+1..$-1]));
+		else
+			return 1000*to!int(s[0..$-1]);
+	} else {
+		return to!int(s);
 	}
-
-	return lst;
 }
 
 
-private string[string] options(string ops)
+DateTime now(string zone="Europe/Moscow")
 {
-	string[string] op;
-	foreach(r; matchAll(ops, ctRegex!("\\s*(.*?)=\"(.*?)\"")))
-		op[r[1]]=r[2];
-	return op;
+	auto tz=PosixTimeZone.getTimeZone(zone);
+	auto t=Clock.currTime(tz);
+	return DateTime(t.year, t.month, t.day, t.hour, t.minute, t.second);
 }
 
-struct Section(string TAG)
+
+//immutable ubyte[string] Months=[
+//	  "января" : 1
+//	, "февраля" : 2
+//	, "марта" : 3
+//	, "апреля" : 4
+//	, "мая" : 5
+//	//, "" : 
+//	//, "" : 
+//	//, "" : 
+//	//, "" : 
+//	//, "" : 
+//	//, "" : 
+//	//, "" : 
+//	//, "" : 
+//	//, "" : 
+//	//, "" : 
+//	//, "" : 
+//	//, "" : 
+//];
+
+DateTime at(string s, DateTime now)
 {
-	string prefix;
-	string[string] opt;
-	Section[] children;
 
-	enum {
-		  begin="<"~TAG~"\\s*"
-		, end="</"~TAG~">"
-		, any=begin~"|"~end
-		, options="(.*?)>"
-	}
+	auto r=matchFirst(s,ctRegex!"сегодня в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, now.month, now.day, to!int(r[1]), to!int(r[2]));
 
+	r=matchFirst(s,ctRegex!"вчера в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, now.month, now.day, to!int(r[1]), to!int(r[2]))-days(1);
 
-	auto opIndex(string key)
-	{
-		auto x=children.filter!(a => "class" in a.opt && a.opt["class"] == key);
-		enforce(!x.empty, "no class \""~key~"\" found in section");
-		return x.front;
-	}
+	r=matchFirst(s,ctRegex!"(\\d+) января в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, 1, to!int(r[1]), to!int(r[2]), to!int(r[3]));
 
-	auto at(string key)
-	{
-		auto x=children.filter!(a => "class" in a.opt && a.opt["class"] == key);
-		if(x.empty) return Section!TAG();
-		return x.front;
-	}
+	r=matchFirst(s,ctRegex!"(\\d+) февраля в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, 2, to!int(r[1]), to!int(r[2]), to!int(r[3]));
 
-	auto list(string key)(string val)
-	{
-		return children.filter!(a => key in a.opt && a.opt[key] == val);
-	}
+	r=matchFirst(s,ctRegex!"(\\d+) марта в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, 3, to!int(r[1]), to!int(r[2]), to!int(r[3]));
 
-	@property string name() const { return ("class" in opt)? opt["class"] : "LAMBDA"; }
-	@property string text() const { string s; foreach(ch; children) s~=ch.prefix; return s; }
+	r=matchFirst(s,ctRegex!"(\\d+) апреля в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, 4, to!int(r[1]), to!int(r[2]), to!int(r[3]));
 
+	r=matchFirst(s,ctRegex!"(\\d+) мая в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, 5, to!int(r[1]), to!int(r[2]), to!int(r[3]));
 
+	r=matchFirst(s,ctRegex!"(\\d+) июня в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, 6, to!int(r[1]), to!int(r[2]), to!int(r[3]));
 
-	void print(string ident ="")
-	{
-		if(children.empty && opt.byKey.empty)
-			return;
+	r=matchFirst(s,ctRegex!"(\\d+) июля в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, 7, to!int(r[1]), to!int(r[2]), to!int(r[3]));
 
-		write(ident, ("class" in opt)? opt["class"] : "LAMBDA", "(");
-		foreach(op; opt.byKey)
-			if(op != "class")
-				write(op, "=", opt[op], " ");
-		writeln(") {");
+	r=matchFirst(s,ctRegex!"(\\d+) августа в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, 8, to!int(r[1]), to!int(r[2]), to!int(r[3]));
 
-		foreach(ch; children)
-			ch.print(ident~"  ");
+	r=matchFirst(s,ctRegex!"(\\d+) сентября в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, 9, to!int(r[1]), to!int(r[2]), to!int(r[3]));
 
-		writeln(ident,"} // ", ("class" in opt)? opt["class"] : "LAMBDA");
-	}
+	r=matchFirst(s,ctRegex!"(\\d+) октября в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, 10, to!int(r[1]), to!int(r[2]), to!int(r[3]));
+
+	r=matchFirst(s,ctRegex!"(\\d+) ноября в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, 11, to!int(r[1]), to!int(r[2]), to!int(r[3]));
+
+	r=matchFirst(s,ctRegex!"(\\d+) декабря в (\\d+):(\\d+)");
+	if(r)
+		return DateTime(now.year, 12, to!int(r[1]), to!int(r[2]), to!int(r[3]));
+
+	enforce(false, s~": unhandled date");
+	return now;
 }
+
 
